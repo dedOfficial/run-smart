ca9fa1acfc2a2a828d460cf93bfb8044dd943321
Konstantin K <karp.karpov981@gmail.com>
2021-04-10 18:27:52 +0300

    fixed adapt promo section bg

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,17 @@ const sass        = require('gulp-sass');
 const rename      = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 // Static server
 gulp.task('server', function() {
     browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
+    gulp.watch('src/*.html').on('change', browserSync.reload);
 });
 
 // // or...
@@ -30,14 +33,41 @@ gulp.task('styles', function() {
         }))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('src/css'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.stream());
 
 });
 
 gulp.task('watch', function () {
-    gulp.watch('src/sass/**/*.+(scss|sass)', gulp.parallel('styles'));
-    gulp.watch('src/*.html').on('change', browserSync.reload);
+    gulp.watch('src/sass/**/*.+(scss|sass|css)', gulp.parallel('styles'));
+    gulp.watch('src/*.html').on('change', gulp.parallel('html'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('html', function () {
+    return gulp.src('src/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('src/js/**/*.js')
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('fonts', function () {
+    return gulp.src('src/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('img', function () {
+    return gulp.src('src/img/**/**/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('mailer', function () {
+    return gulp.src('src/mailer/**/*')
+        .pipe(gulp.dest('dist/mailer'));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'img', 'mailer', 'html'));
